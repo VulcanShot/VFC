0c56f4213d841ee1f385775791e1504c01b3769c
VulcanShot <pieromarcelohp@gmail.com>
2022-01-16 01:15:31 -0500

    Initial Commit

---
+++ source/VulcanFlagCreator/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace VulcanFlagCreator
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+    }
+}
